c81d0f6c58687aea89f5ef4394f8f59d1c9428b0
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2024-12-23 19:42:21 +0200

    Edited test file

--- src/test/test_arc_consistency.py
+++ src/test/test_arc_consistency.py
@@ -1,39 +1,45 @@
+import unittest
 import numpy as np
 from src.solver.csp import SudokuCSP
 from src.solver.backtracking import Backtracking
 import time
 
-class TestSudokuSolver:
-    def TestSudokuSolver(sudoku_grid, expected_solution, level):
-        print(f"Testing Sudoku solver for level: {level}")
-        sudoku_csp = SudokuCSP(sudoku_grid)
-        start = time.time()
-        solution = Backtracking(sudoku_csp).backtrackingSearch()
-        end = time.time()
+"""This script tests the Sudoku solver using the Backtracking algorithm.
+    to run the script, run the following command:
+    python -m unittest src.test.test_sudoku_solver.py """
 
-        print(f"Time taken: {end - start:.2f} sec")
 
-        if solution:
-            solved_grid = np.zeros((9, 9), dtype=int)
-            for (i, j), value in solution.items():
-                solved_grid[i, j] = value
+def test_sudoku_solver(sudoku_grid, expected_solution, level):
+    print(f"Testing Sudoku solver for level: {level}")
+    sudoku_csp = SudokuCSP(sudoku_grid)
+    start = time.time()
+    solution = Backtracking(sudoku_csp).backtrackingSearch()
+    end = time.time()
 
-            print("Solution:")
-            for row in solved_grid:
-                print(row)
+    print(f"Time taken: {end - start:.2f} sec")
 
-            if np.array_equal(solved_grid, expected_solution):
-                print("Test passed!")
-            else:
-                print("Test failed!")
+    if solution:
+        solved_grid = np.zeros((9, 9), dtype=int)
+        for (i, j), value in solution.items():
+            solved_grid[i, j] = value
+
+        print("Solution:")
+        for row in solved_grid:
+            print(row)
+
+        if np.array_equal(solved_grid, expected_solution):
+            print("Test passed!")
         else:
-            print("No solution found.")
-        print("\n")
+            print("Test failed!")
+    else:
+        print("No solution found.")
+    print("\n")
 
-    def run_tests():
-        # Level: easy
-        # time: 0.21 sec
 
+class TestSudokuSolver(unittest.TestCase):
+    # Level: easy
+    # time: 0.21 sec
+    def test_easy(self):
         sudoku_grid_easy = np.array(
             [
                 [0, 0, 1, 0, 3, 4, 0, 0, 6],
@@ -60,11 +66,11 @@ class TestSudokuSolver:
                 [6, 8, 3, 4, 2, 5, 9, 1, 7],
             ]
         )
-        TestSudokuSolver(sudoku_grid_easy, expected_solution_easy, "easy")
+        test_sudoku_solver(sudoku_grid_easy, expected_solution_easy, "easy")
 
-        # Level: medium
-        # time: 0.31 sec
-
+    # Level: medium
+    # time: 0.31 sec
+    def test_medium(self):
         sudoku_grid_medium = np.array(
             [
                 [6, 0, 0, 0, 3, 0, 0, 4, 8],
@@ -91,11 +97,11 @@ class TestSudokuSolver:
                 [9, 6, 4, 3, 5, 2, 8, 7, 1],
             ]
         )
-        TestSudokuSolver(sudoku_grid_medium, expected_solution_medium, "medium")
+        test_sudoku_solver(sudoku_grid_medium, expected_solution_medium, "medium")
 
-        # Level: hard
-        # time: 0.31 sec
-
+    # Level: hard
+    # time: 0.31 sec
+    def test_hard(self):
         sudoku_grid_hard = np.array(
             [
                 [0, 0, 0, 0, 0, 8, 0, 6, 0],
@@ -122,11 +128,11 @@ class TestSudokuSolver:
                 [4, 9, 2, 3, 6, 7, 5, 1, 8],
             ]
         )
-        TestSudokuSolver(sudoku_grid_hard, expected_solution_hard, "hard")
+        test_sudoku_solver(sudoku_grid_hard, expected_solution_hard, "hard")
 
-        # Level: master
-        # time: 0.43 sec
-
+    # Level: master
+    # time: 0.43 sec
+    def test_master(self):
         sudoku_grid_master = np.array(
             [
                 [1, 0, 0, 5, 0, 0, 6, 0, 0],
@@ -153,11 +159,11 @@ class TestSudokuSolver:
                 [6, 1, 2, 4, 7, 5, 8, 3, 9],
             ]
         )
-        TestSudokuSolver(sudoku_grid_master, expected_solution_master, "master")
+        test_sudoku_solver(sudoku_grid_master, expected_solution_master, "master")
 
-        # Level: extreme
-        # time: 1.27 sec
-
+    # Level: extreme
+    # time: 1.27 sec
+    def test_extreme_1(self):
         sudoku_grid_extreme = np.array(
             [
                 [6, 0, 8, 0, 0, 0, 0, 0, 0],
@@ -184,12 +190,12 @@ class TestSudokuSolver:
                 [1, 9, 4, 7, 8, 2, 6, 3, 5],
             ]
         )
-        TestSudokuSolver(sudoku_grid_extreme, expected_solution_extreme, "extreme")
+        test_sudoku_solver(sudoku_grid_extreme, expected_solution_extreme, "extreme")
 
-        # Level: extreme
-        # time: 0.35 sec
-
-        sudoku_grid_extreme_2 = np.array(
+    # Level: extreme
+    # time: 0.35 sec
+    def test_extreme_2(self):
+        sudoku_grid_extreme = np.array(
             [
                 [8, 2, 0, 5, 0, 0, 3, 6, 0],
                 [0, 0, 0, 0, 0, 9, 0, 5, 0],
@@ -202,7 +208,7 @@ class TestSudokuSolver:
                 [0, 0, 9, 0, 0, 0, 6, 0, 0],
             ]
         )
-        expected_solution_extreme_2 = np.array(
+        expected_solution_extreme = np.array(
             [
                 [8, 2, 1, 5, 7, 4, 3, 6, 9],
                 [6, 4, 3, 1, 8, 9, 2, 5, 7],
@@ -216,10 +222,11 @@ class TestSudokuSolver:
             ]
         )
 
-        TestSudokuSolver(sudoku_grid_extreme_2, expected_solution_extreme_2, "extreme")
+        test_sudoku_solver(sudoku_grid_extreme, expected_solution_extreme, "extreme")
 
-        # Level: hardest sudoku in the world
-        # time: 15.19 sec
+    # Level: hardest sudoku in the world
+    # time: 15.19 sec
+    def test_insane(self):
         sudoku_grid_insane = np.array(
             [
                 [8, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -246,4 +253,8 @@ class TestSudokuSolver:
                 [7, 9, 6, 3, 1, 8, 4, 5, 2],
             ]
         )
-        TestSudokuSolver(sudoku_grid_insane, expected_solution_insane, "insane")
+        test_sudoku_solver(sudoku_grid_insane, expected_solution_insane, "insane")
+
+
+if __name__ == "__main__":
+    unittest.main()
