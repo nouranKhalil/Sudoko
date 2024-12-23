8c1aa5cee5f31ddc09ad1b5c89d78b624831735a
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2024-12-23 06:05:08 +0200

    Added arc consistency/csp sudoku

--- src/solver/arc_consistency.py
+++ src/solver/arc_consistency.py
@@ -1,8 +1,59 @@
-class ArcConsistency:
-    def __init__(self):
-        pass
-    def testImport(self):
-        print("test arc consistency")
+from csp import SudokuCSP
+from collections import deque
 
-test = ArcConsistency()
-test.testImport()
+class ArcConsistency:
+    def __init__(self, csp: SudokuCSP):
+        self.csp = csp
+
+    def testImport(self):
+        print(self.csp.variables)
+
+    def ARC_3(self) -> bool:
+        queue = deque()
+        queue.extend(self.csp.constraints)
+        while queue:
+            X_i, X_j = queue.popleft()
+            if self.revise(X_i, X_j):
+                if len(self.csp.domains[X_i]) == 0:
+                    return False
+                for X_k in self.csp.constraints:
+                    if X_k[0] == X_i and X_k[1] != X_j:
+                        queue.append(X_k)
+        return True
+
+    def revise(self, X_i: tuple, X_j: tuple) -> bool:
+        revised = False
+        to_remove = []
+        for x in self.csp.domains[X_i]:
+            if not any(y != x for y in self.csp.domains[X_j]):
+                to_remove.append(x)
+                revised = True
+        for x in to_remove:
+            self.csp.domains[X_i].remove(x)
+        return revised
+
+
+sudoku = SudokuCSP()
+
+sudoku.print_domains()
+sudoku_grid = [
+    [8, 0, 9, 5, 0, 1, 7, 3, 6],
+    [2, 0, 7, 0, 6, 3, 0, 0, 0],
+    [1, 6, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 4, 0, 7],
+    [0, 9, 0, 3, 0, 7, 0, 2, 0],
+    [7, 0, 6, 0, 8, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 6, 3],
+    [0, 0, 0, 9, 3, 0, 5, 0, 2],
+    [5, 3, 2, 6, 0, 4, 8, 0, 9],
+]
+sudoku.update_domains(sudoku_grid)
+print("after update")
+sudoku.print_domains()
+
+arc_consistency = ArcConsistency(sudoku)
+
+
+arc_consistency.ARC_3()
+print("after arc consistency")
+arc_consistency.csp.print_domains()
--- src/solver/csp.py
+++ src/solver/csp.py
@@ -3,10 +3,67 @@ class CSP:
         self.variables = variables
         self.domains = domains
         self.constraints = constraints
-        
-    
+
     def isConsistent(self, variable, value, assignment):
         for neighbor in self.constraints[variable]:
             if neighbor in assignment and assignment[neighbor] == value:
                 return False
         return True
+
+
+# Remember to change the grid to numpy array in all the project for optimaisation
+
+
+class SudokuCSP:
+    def __init__(self):
+        self.variables = [(i, j) for i in range(9) for j in range(9)]
+        self.domains = {variable: set(range(1, 10)) for variable in self.variables}
+        self.constraints = [
+            (X1, X2)
+            for X1 in self.variables
+            for X2 in self.variables
+            if (
+                (X1 != X2)
+                and (
+                    X1[0] == X2[0]
+                    or X1[1] == X2[1]
+                    or (X1[0] // 3 == X2[0] // 3 and X1[1] // 3 == X2[1] // 3)
+                )
+            )
+        ]
+
+    def update_domains(self, sudoku_grid: list) -> None:
+        """
+        Update the domains of the variables based on the given Sudoku grid.
+
+        Parameters:
+        sudoku_grid (list): A 2D list representing the Sudoku grid where 0 indicates an empty cell.
+        """
+        for variable in self.variables:
+            x, y = variable
+            if sudoku_grid[x][y] != 0:
+                self.domains[variable] = {sudoku_grid[x][y]}
+
+    def print_domains(self) -> None:
+        # print(self.domains)
+        for key, value in self.domains.items():
+            print(f"{key}: {value}")
+
+
+sudoku = SudokuCSP()
+
+sudoku.print_domains()
+sudoku_grid = [
+    [8, 0, 9, 5, 0, 1, 7, 3, 6],
+    [2, 0, 7, 0, 6, 3, 0, 0, 0],
+    [1, 6, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 0, 4, 0, 7],
+    [0, 9, 0, 3, 0, 7, 0, 2, 0],
+    [7, 0, 6, 0, 8, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 6, 3],
+    [0, 0, 0, 9, 3, 0, 5, 0, 2],
+    [5, 3, 2, 6, 0, 4, 8, 0, 9],
+]
+sudoku.update_domains(sudoku_grid)
+print("after update")
+sudoku.print_domains()
